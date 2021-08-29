5a0d628bebbae77fd6fdc9173cc07b8db1387f23
tank537285 <tank537285@gmail.com>
2021-08-29 13:57:08 +0800

    no message

--- bin/miguelrc.js
+++ bin/miguelrc.js
@@ -1,2 +1,39 @@
 #! /usr/bin/env node
-console.log(123)
+const { program } = require('commander');
+const inquirer = require('inquirer');
+program.version('0.0.1');
+program
+    .option('-d, --debug', 'output extra debugging')
+    .option('-n, --name <type>', '设置项目名字');
+
+program.parse(process.argv);
+const options = program.opts();
+
+
+/*针对有选项*/
+inquirer
+    .prompt([
+        /* Pass your questions in here */
+    ])
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+
+if(options.name) {
+    const git = program.command('git');
+    git
+        .command(`clone https://github.com/tank537285/miguelsc/tree/main/packages/miguelrc ./${name}`)
+        .action(() => {
+            console.log('brew tea');
+        });
+};
+
+
+
